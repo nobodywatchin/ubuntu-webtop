58d76dd1706a99f78cbb4afc2caa9ad248fe2845
mollomm1 <68751598+Mollomm1@users.noreply.github.com>
2024-04-01 16:54:27 +0000

    stuff

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 setterm blank 0
 setterm powerdown 0
-export XDG_CURRENT_DESKTOP=GNOME
 export XDG_SESSION_TYPE=x11
+export DESKTOP_SESSION=ubuntu
+export GNOME_SHELL_SESSION_MODE=ubuntu
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME
 sudo service dbus start
 gnome-shell --x11 -r > /dev/null 2>&1
